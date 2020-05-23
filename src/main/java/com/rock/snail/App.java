eb6f76bb5eefcab70fb4621368adb5e27bf42e6c
rocksnaillm <58380911+rocksnaillm@users.noreply.github.com>
2020-05-23 16:15:05 +0800

    first commit

---
+++ src/main/java/com/rock/snail/App.java
@@ -0,0 +1,13 @@
+package com.rock.snail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
