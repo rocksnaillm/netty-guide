70e8bdc8dcf7a2f01f807f11cc71bbc6c2ca6ca5
rocksnaillm <58380911+rocksnaillm@users.noreply.github.com>
2020-09-26 17:37:33 +0800

    netty 模拟http

---
+++ src/main/java/com/rock/snail/netty/http/HttpServer.java
@@ -0,0 +1,21 @@
+package com.rock.snail.netty.http;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+
+public class HttpServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+        try {
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/rock/snail/netty/http/HttpServerInitializer.java
@@ -0,0 +1,11 @@
+package com.rock.snail.netty.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+
+    }
+}
