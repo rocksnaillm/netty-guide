c787301131d9120953ae52303880bcfb5e6cf46c
rocksnaillm <58380911+rocksnaillm@users.noreply.github.com>
2020-09-28 16:49:37 +0800

    websocket测试

---
+++ src/main/java/com/rock/snail/netty/websocket/MyServer.java
@@ -0,0 +1,62 @@
+package com.rock.snail.netty.websocket;
+
+import com.rock.snail.netty.simple.NettyServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+public class MyServer {
+
+    public static void main(String[] args) throws Exception{
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class) // 使用 NioServerSocketChannel 作为服务IQ通道实现
+                    .option(ChannelOption.SO_BACKLOG,128)
+                    .childOption(ChannelOption.SO_KEEPALIVE,true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new HttpServerCodec());
+                            ch.pipeline().addLast(new ChunkedWriteHandler());
+                            ch.pipeline().addLast(new HttpObjectAggregator(8192));
+                            ch.pipeline().addLast(new WebSocketServerProtocolHandler("/hello"));
+                            ch.pipeline().addLast(new MyTestWebSocketFrameHandler());
+                        }
+                    });
+
+            System.out.println("is ready");
+            ChannelFuture channelFuture = bootstrap.bind(7000).sync();
+            channelFuture.addListener(new GenericFutureListener<Future<? super Void>>() {
+                @Override
+                public void operationComplete(Future<? super Void> future) throws Exception {
+                    if(channelFuture.isSuccess()){
+                        System.out.println("监听 7000 端口成功");
+                    }else {
+                        System.out.println("监听 6666 端口失败");
+                    }
+                }
+            });
+
+
+            channelFuture.channel().closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/rock/snail/netty/websocket/MyTestWebSocketFrameHandler.java
@@ -0,0 +1,31 @@
+package com.rock.snail.netty.websocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDate;
+
+public class MyTestWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("服务器接收消息: " + msg.text());
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务器时间:" + LocalDate.now() + " "+msg.text()));
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded 被调用："+ctx.channel().id().asLongText());
+        System.out.println("handlerAdded 被调用："+ctx.channel().id().asShortText());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved 被调用："+ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("异常发生:" + cause.getMessage());
+    }
+}
