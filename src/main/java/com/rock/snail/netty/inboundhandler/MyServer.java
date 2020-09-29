d85d103f1bbbeca0a1045c51464112e29dc5a7fc
rocksnaillm <58380911+rocksnaillm@users.noreply.github.com>
2020-09-29 18:00:34 +0800

    编码器

---
+++ src/main/java/com/rock/snail/netty/inboundhandler/MyServer.java
@@ -0,0 +1,44 @@
+package com.rock.snail.netty.inboundhandler;
+
+import com.rock.snail.netty.groupchat.GroupChatServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class MyServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);//1
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();//8
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,128)
+                    .childOption(ChannelOption.SO_KEEPALIVE,true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("decoder",new StringDecoder());
+                            ch.pipeline().addLast("encoder",new StringEncoder());
+                            ch.pipeline().addLast(new GroupChatServerHandler());
+                        }
+                    });
+
+            System.out.println("netty 服务器准备启动");
+            ChannelFuture channelFuture = bootstrap.bind(7000).sync();
+            channelFuture.channel().close().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
